143ccf6373a93d5052513dbf90c7a18b5675bdb5
Pooja Gangwar <pooja.gangwar@cyberark.com>
2022-03-31 18:19:38 +0530

    initial changes

--- tests/unit/plugins/lookup/test_conjur_variable.py
+++ tests/unit/plugins/lookup/test_conjur_variable.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import, division, print_function
 __metaclass__ = type
 
 from ansible_collections.cyberark.conjur.tests.unit.compat.unittest import TestCase
-from ansible_collections.cyberark.conjur.tests.unit.compat.mock import patch , MagicMock
+from ansible_collections.cyberark.conjur.tests.unit.compat.mock import patch, MagicMock
 from ansible_collections.cyberark.conjur.plugins.lookup.conjur_variable import _merge_dictionaries
 from ansible.plugins.loader import lookup_loader
 
@@ -16,10 +16,10 @@ class Test(TestCase):
     def setUp(self):
         self.lookup = lookup_loader.get("plugins.lookup.conjur_variable")
 
-    @patch('ansible_collections.cyberark.conjur.plugins.lookup.conjur_variable._merge_dictionaries',MockSecretsVault())
+    @patch('ansible_collections.cyberark.conjur.plugins.lookup.conjur_variable._merge_dictionaries', MockSecretsVault())
     def test_get_secret_json(self):
-        functionOutput= _merge_dictionaries(
+        functionOutput = _merge_dictionaries(
             {},
             {'id': 'host/ansible/ansible-master', 'api_key': '1j8t1rx2ghwhx7392wt1h14qn3c22yp4w2y395yk9d2hz8gvb1nbh6yg'}
         )
-        self.assertEquals(MockSecretsVault.RESPONSE,functionOutput)
+        self.assertEquals(MockSecretsVault.RESPONSE, functionOutput)
