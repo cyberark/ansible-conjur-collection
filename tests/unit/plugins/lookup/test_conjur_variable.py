b0ddb334a58052085d146957819112433615fed1
Pooja Gangwar <56577577+Pooja-Gangwar@users.noreply.github.com>
2022-04-01 20:11:08 +0530

    Update test_conjur_variable.py

--- tests/unit/plugins/lookup/test_conjur_variable.py
+++ tests/unit/plugins/lookup/test_conjur_variable.py
@@ -3,7 +3,7 @@ __metaclass__ = type
 
 from ansible_collections.cyberark.conjur.tests.unit.compat.unittest import TestCase
 from ansible_collections.cyberark.conjur.tests.unit.compat.mock import patch, MagicMock
-from ansible_collections.cyberark.conjur.plugins.lookup.conjur_variable import _merge_dictionaries
+from ansible_collections.cyberark.conjur.plugins.lookup.conjur_variable import _merge_dictionaries , _fetch_conjur_token
 from ansible.plugins.loader import lookup_loader
 
 
@@ -25,3 +25,18 @@ class Test(TestCase):
             {'id': 'host/ansible/ansible-master', 'api_key': '1j8t1rx2ghwhx7392wt1h14qn3c22yp4w2y395yk9d2hz8gvb1nbh6yg'}
         )
         self.assertEquals(MockSecretsVault.RESPONSE, functionOutput)
+        
+    @patch('ansible_collections.cyberark.conjur.plugins.lookup.conjur_variable.open_url')
+    def test_fetch_conjur_token(self, mock_open_url):
+        mock_response = MagicMock()
+        mock_response.getcode.return_value = 200
+        mock_response.read.return_value = "response body"
+        mock_open_url.return_value = mock_response
+        result = _fetch_conjur_token("url", "account", "username", "api_key", True, "cert_file")
+        mock_open_url.assert_called_with("url/authn/account/username/authenticate",
+                                         data="api_key",
+                                         method="POST",
+                                         validate_certs=True,
+                                         ca_path="cert_file")
+        self.assertEquals("response body", result)
+        
