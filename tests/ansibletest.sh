23402ac7afc7bc87f2d343d6224ebed089bc3288
Pooja Gangwar <56577577+Pooja-Gangwar@users.noreply.github.com>
2022-04-08 20:43:57 +0530

    Update ansibletest.sh

--- tests/ansibletest.sh
+++ tests/ansibletest.sh
@@ -1,4 +1,11 @@
 #!/bin/bash -ex
+cd ../../
+mkdir -p ansible_collections/cyberark/conjur
+cp -r ansible-conjur-collection/ ansible_collections/cyberark/conjur
+cd ansible_collections/cyberark/conjur
+
 pip install https://github.com/ansible/ansible/archive/stable-2.9.tar.gz --disable-pip-version-check
 ansible-test units --docker default -v --python 3.8 --coverage
 ansible-test coverage html -v --requirements --group-by command --group-by version
+
+cp -r ansible-conjur-collection/ ansible_collections/cyberark/conjur
