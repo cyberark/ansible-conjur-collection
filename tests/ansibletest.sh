5b1fa77463fa4e62b7c43e458c4651dd763bf4f2
Pooja Gangwar <pooja.gangwar@cyberark.com>
2022-04-11 23:10:33 +0530

    initial changes

--- tests/ansibletest.sh
+++ tests/ansibletest.sh
@@ -8,9 +8,14 @@ mv ansible-conjur-collection conjur
 cd conjur
 # pip install --user git+https://github.com/pygobject/pycairo.git
 pip install https://github.com/ansible/ansible/archive/stable-2.10.tar.gz --disable-pip-version-check
-ansible-galaxy collection install ansible.netcommon ansible.utils -p .
-ansible-test units -v --color --docker --coverage
-# ansible-test units --docker default -v --python 3.8 --coverage
+
+# sudo apt install libgirepository1.0-dev gcc libcairo2-dev pkg-config python3-dev gir1.2-gtk-3.0
+# pip3 install pycairo
+# pip3 install PyGObject
+
+# ansible-galaxy collection install ansible.netcommon ansible.utils -p .
+# ansible-test units -v --color --docker --coverage
+ansible-test units --docker default -v --python 3.8 --coverage
 ansible-test coverage html -v --requirements --group-by command --group-by version
 # ansible-test coverage html
 
