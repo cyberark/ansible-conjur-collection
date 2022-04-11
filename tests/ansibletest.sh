ba4dabf43f4c4017d2ed943a087b5158845fea0a
Pooja Gangwar <56577577+Pooja-Gangwar@users.noreply.github.com>
2022-04-11 16:19:59 +0530

    Update ansibletest.sh

--- tests/ansibletest.sh
+++ tests/ansibletest.sh
@@ -6,9 +6,10 @@ git clone --single-branch --branch deleteitansibletest https://github.com/cybera
 
 mv ansible-conjur-collection conjur
 cd conjur
-
+# pip install --user git+https://github.com/pygobject/pycairo.git
 pip install https://github.com/ansible/ansible/archive/stable-2.10.tar.gz --disable-pip-version-check
 ansible-test units --docker default -v --python 3.8 --coverage
-ansible-test coverage html -v --requirements --group-by command --group-by version
+# ansible-test coverage xml -v --requirements --group-by command --group-by version
+ansible-test coverage html
 
 
