2a73be85f4cf03d7111f3ecdf189c93a4f9a7ac3
Pooja Gangwar <pooja.gangwar@cyberark.com>
2022-04-11 22:13:49 +0530

    initial changes

--- tests/ansibletest.sh
+++ tests/ansibletest.sh
@@ -8,7 +8,9 @@ mv ansible-conjur-collection conjur
 cd conjur
 # pip install --user git+https://github.com/pygobject/pycairo.git
 pip install https://github.com/ansible/ansible/archive/stable-2.10.tar.gz --disable-pip-version-check
-ansible-test units --docker default -v --python 3.8 --coverage
+ansible-galaxy collection install ansible.netcommon ansible.utils -p .
+ansible-test units -v --color --docker --coverage
+# ansible-test units --docker default -v --python 3.8 --coverage
 ansible-test coverage html -v --requirements --group-by command --group-by version
 # ansible-test coverage html
 
