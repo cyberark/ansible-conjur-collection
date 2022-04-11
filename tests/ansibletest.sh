a394291a5050632d6c6cdd86feeb872875aca56a
Pooja Gangwar <pooja.gangwar@cyberark.com>
2022-04-11 13:50:17 +0530

    initial changes

--- tests/ansibletest.sh
+++ tests/ansibletest.sh
@@ -1,4 +1,14 @@
 #!/bin/bash -ex
+
+#  docker-compose up -d --build
+cd ../../
+mkdir -p ansible_collections/cyberark/conjur
+
+cp -r ansible-conjur-collection/ ansible_collections/cyberark/conjur
+cd ansible_collections/cyberark/conjur
+
 pip install https://github.com/ansible/ansible/archive/stable-2.9.tar.gz --disable-pip-version-check
 ansible-test units --docker default -v --python 3.8 --coverage
 ansible-test coverage html -v --requirements --group-by command --group-by version
+
+
