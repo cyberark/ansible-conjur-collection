a32887ff87b6e4630e49f69782037d35e2b5e18c
Pooja Gangwar <pooja.gangwar@cyberark.com>
2022-04-12 18:34:56 +0530

    initial changes

--- tests/ansibletest.sh
+++ tests/ansibletest.sh
@@ -6,6 +6,7 @@ git clone --single-branch --branch to_testonly https://github.com/cyberark/ansib
 
 mv ansible-conjur-collection conjur
 cd conjur
+pip install pycairo
 # pip install --user git+https://github.com/pygobject/pycairo.git
 
 # export PATH="${PATH}:/Users/caio.hc.oliveira/Library/Python/3.7/bin"
@@ -14,7 +15,7 @@ cd conjur
 pip install https://github.com/ansible/ansible/archive/stable-2.10.tar.gz --disable-pip-version-check
 
 # sudo apt install libgirepository1.0-dev gcc libcairo2-dev pkg-config python3-dev gir1.2-gtk-3.0
-pip install pycairo
+
 # pip3 install PyGObject
 
 # ansible-galaxy collection install ansible.netcommon ansible.utils -p .
