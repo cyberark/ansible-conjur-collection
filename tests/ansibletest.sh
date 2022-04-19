1d5057c1dd80d02d7e8d36ce028333ec4c382f7f
Pooja Gangwar <pooja.gangwar@cyberark.com>
2022-04-19 17:28:15 +0530

    initial changes

--- tests/ansibletest.sh
+++ tests/ansibletest.sh
@@ -1,14 +1,14 @@
 #!/bin/bash -eu
 
-mkdir -p ansible_collections/cyberark/
-cd ansible_collections/cyberark/
-git clone --single-branch --branch test_it https://github.com/cyberark/ansible-conjur-collection.git
+# mkdir -p ansible_collections/cyberark/
+# cd ansible_collections/cyberark/
+# git clone --single-branch --branch test_it https://github.com/cyberark/ansible-conjur-collection.git
 
-mv ansible-conjur-collection conjur
-cd conjur
+# mv ansible-conjur-collection conjur
+# cd conjur
 
-# pip install pycairo
-pip install https://github.com/ansible/ansible/archive/stable-2.10.tar.gz --disable-pip-version-check
+# # pip install pycairo
+# pip install https://github.com/ansible/ansible/archive/stable-2.10.tar.gz --disable-pip-version-check
 
-ansible-test units --docker default -v --python 3.8 --coverage
-ansible-test coverage html -v --requirements --group-by command --group-by version
+# ansible-test units --docker default -v --python 3.8 --coverage
+# ansible-test coverage html -v --requirements --group-by command --group-by version
