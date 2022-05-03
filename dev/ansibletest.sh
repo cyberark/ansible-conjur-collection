9f9e7c8182160c84345a01c6396c86831da463f5
Pooja Gangwar <pooja.gangwar@cyberark.com>
2022-05-03 14:00:01 +0530

    initial cahnges

--- dev/ansibletest.sh
+++ dev/ansibletest.sh
@@ -15,18 +15,15 @@ cd ansible_collections/cyberark/
 git clone --single-branch --branch TestingPurposeOnly https://github.com/cyberark/ansible-conjur-collection.git
 mv ansible-conjur-collection conjur
 cd conjur
-echo "Warning: Pooja 1"
-pwd
-ls
+
 # pip install pycairo
+export PATH=/var/lib/jenkins/.local/bin:$PATH
 pip install https://github.com/ansible/ansible/archive/stable-2.10.tar.gz --disable-pip-version-check
 ansible-test units --docker default -v --python 3.8 tests/unit/plugins/lookup/test_conjur_variable.py
-echo "Warning: Pooja 2"
-pwd
-ls
+
 ansible-test units --docker default -v --python 3.8 --coverage
 ansible-test coverage html -v --requirements --group-by command --group-by version
-echo "Warning: Pooja 3"
+
 cd ../../../
 cp -r ansible_collections/cyberark/conjur/tests/output  ansible-conjur-collection/tests/
 rm -rf ansible_collections
