4f2c1f20d6f3772b20d84a5e1ff0ef109c9c3a1c
Pooja Gangwar <56577577+Pooja-Gangwar@users.noreply.github.com>
2022-05-10 00:20:59 +0530

    Update ansibletest.sh

--- dev/ansibletest.sh
+++ dev/ansibletest.sh
@@ -1,33 +1,46 @@
 #!/bin/bash -eu
 
-cd ../../
 
+filepath=$(pwd)
+firstthree=${filepath:1:3}
+
+if [ "$firstthree" == var ]; then
+   currentbranch=$BRANCH_NAME
+else
+   currentbranch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
+fi
+
+
+cd ../../
 DIR="ansible-conjur-collection/tests/output"
 if [ -d "$DIR" ]; then
    echo "Existing '$DIR' found"
    rm -rf ansible-conjur-collection/tests/output
-   echo "'$DIR' Directory has been deleted"
 else
-   echo "Warning: '$DIR' NOT found. "
+   echo "'$DIR' NOT found. "
 fi
 
 mkdir -p ansible_collections/cyberark/
 cd ansible_collections/cyberark/
-git clone --single-branch --branch ONYX-15263 https://github.com/cyberark/ansible-conjur-collection.git
+git clone --single-branch --branch "$currentbranch" https://github.com/cyberark/ansible-conjur-collection.git
 mv ansible-conjur-collection conjur
 cd conjur
 
 # pip install pycairo
 export PATH=/var/lib/jenkins/.local/bin:$PATH
 pip install https://github.com/ansible/ansible/archive/devel.tar.gz --disable-pip-version-check
-ansible-test units --docker default -v --python 3.8 --coverage
+ansible-test units --docker default -v --python 3.8 tests/unit/plugins/lookup/test_conjur_variable.py --coverage
 ansible-test coverage html -v --requirements --group-by command --group-by version
-
 cd ../../../
-DIR_exists="workspace"
-if [ ! -d "$DIR_exists" ]; then
-    cp -r ansible_collections/cyberark/conjur/tests/output ansible-conjur-collection/tests
 
+CURRENTDIR="workspace"
+if [ -d "$CURRENTDIR" ]; then
+   rootdir="_-ansible-conjur-collection_"
+   Combinedstring=$rootdir$currentbranch
+   get32characters=${Combinedstring: -32}
+   cp -r ansible_collections/cyberark/conjur/tests/output workspace/"$get32characters"/tests
+else
+   cp -r ansible_collections/cyberark/conjur/tests/output ansible-conjur-collection/tests
 fi
 
 rm -rf ansible_collections
