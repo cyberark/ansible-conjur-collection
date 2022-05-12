1eb5183157a6ddd86db804570d3b1b655052fd74
Pooja Gangwar <pooja.gangwar@cyberark.com>
2022-05-12 15:08:30 +0530

    initial changes

--- tests/conjur_variable/test_with_enterprise.sh
+++ tests/conjur_variable/test_with_enterprise.sh
@@ -83,10 +83,19 @@ echo " Setup CLI "
       -c "cp /root/conjur-demo.pem conjur-enterprise.pem"
   echo " =======4====="
   cp conjur-enterprise.pem ../
+
+
+
+
+
+
+    # ANSIBLE_CONJUR_CERT_FILE='/cyberark/tests/conjur.pem'
 echo " =======5====="
 # CONJUR_ADMIN_AUTHN_API_KEY=$(docker-compose exec -T conjur conjurctl role retrieve-key cucumber:user:admin)
   # CONJUR_ADMIN_AUTHN_API_KEY=$(./bin/cli conjur role retrieve-key cucumber:user:admin)
-  echo " =======55== ${CONJUR_ADMIN_AUTHN_API_KEY}==="
+  ANSIBLE_MASTER_AUTHN_API_KEY=$(./bin/cli conjur_cli conjur host rotate_api_key --host ansible/ansible-master)
+  echo " =======55== ${ANSIBLE_MASTER_AUTHN_API_KEY}==="
+
   CONJUR_ADMIN_AUTHN_API_KEY="$(./bin/cli conjur user rotate_api_key|tail -n 1| tr -d '\r')"
   echo "admin api key: ${CONJUR_ADMIN_AUTHN_API_KEY}"
   echo "${CONJUR_ADMIN_AUTHN_API_KEY}" > api_key
