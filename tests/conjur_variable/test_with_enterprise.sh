5c3da7c7c341f1b817cd04fc25fcbd2e78fe61b2
Pooja Gangwar <56577577+Pooja-Gangwar@users.noreply.github.com>
2022-05-13 13:41:17 +0530

    Update test_with_enterprise.sh

--- tests/conjur_variable/test_with_enterprise.sh
+++ tests/conjur_variable/test_with_enterprise.sh
@@ -1,20 +1,11 @@
 #!/bin/bash -eu
 
-# set -o pipefail
 
-# function cleanup {
-#   echo 'Removing test environment'
-#   echo '---'
-#   docker-compose down -v
-# }
-
-# trap cleanup EXIT
-
-# cleanup
 
 # normalises project name by filtering non alphanumeric characters and transforming to lowercase
 declare -x COMPOSE_PROJECT_NAME
 COMPOSE_PROJECT_NAME=$(echo "${BUILD_TAG:-ansible-plugin-testing}-conjur-variable" | sed -e 's/[^[:alnum:]]//g' | tr '[:upper:]' '[:lower:]')
+export COMPOSE_PROJECT_NAME
 
 declare -x ANSIBLE_MASTER_AUTHN_API_KEY=''
 declare -x CONJUR_ADMIN_AUTHN_API_KEY=''
@@ -27,40 +18,26 @@ function main() {
 
 echo " Step 1 "
 
-
 git clone --single-branch --branch main https://github.com/conjurdemos/conjur-intro.git
 cd conjur-intro
 echo " Provision Master"
   ./bin/dap --provision-master
   ./bin/dap --provision-follower
-echo " Step 2 "
 
 echo " Setup Policy "
   # cp ../policy/root.yml .
 
   cd ..
-echo " Step 3 "
   cp -r tests/conjur_variable/policy/root.yml conjur-intro/
-  echo " Step 4 "
-
   cd conjur-intro
-echo " Step 5 "
 
-  # ./bin/cli conjur policy load --replace root root.yml
-  # echo " =======1====="
-  # ./bin/cli conjur variable values add ansible/test-secret test_secret_password
-  #  echo " =======2====="
-  # ./bin/cli conjur variable values add ansible/test-secret-in-file test_secret_in_file_password
-  #  echo " =======3====="
-  # ./bin/cli conjur variable values add ansible/target-password target_secret_password
-
-    echo " =======1====="
+    echo " ========load policy====="
     ./bin/cli conjur policy load root root.yml
-    echo " =======2====="
+    echo " ========Set Variable value ansible/test-secret ====="
     ./bin/cli conjur variable values add ansible/test-secret test_secret_password
-    echo " =======3====="
+     echo " =======Set Variable value ansible/test-secret-in-file ====="
     ./bin/cli conjur variable values add ansible/test-secret-in-file test_secret_in_file_password
-    echo " =======4====="
+     echo " =======Set Variable value ansible/var with spaces ====="
     # ./bin/cli conjur variable values add "ansible/var with spaces" var_with_spaces_secret_password
 
 
@@ -76,15 +53,9 @@ echo " Setup CLI "
       -c "cp /root/conjur-demo.pem conjur-enterprise.pem
       conjur host rotate_api_key --host ansible/ansible-master
       "
-  echo " =======4====="
+
   cp conjur-enterprise.pem ../
 
-
-    # ANSIBLE_CONJUR_CERT_FILE='/cyberark/tests/conjur.pem'
-echo " =======5====="
-# CONJUR_ADMIN_AUTHN_API_KEY=$(docker-compose exec -T conjur conjurctl role retrieve-key cucumber:user:admin)
-  # CONJUR_ADMIN_AUTHN_API_KEY=$(./bin/cli conjur role retrieve-key cucumber:user:admin)
-  # ANSIBLE_MASTER_AUTHN_API_KEY=$(./bin/cli conjur host rotate_api_key --host ansible/ansible-master)
   echo " =======55====="
 
   CONJUR_ADMIN_AUTHN_API_KEY="$(./bin/cli conjur user rotate_api_key|tail -n 1| tr -d '\r')"
@@ -93,13 +64,10 @@ echo " =======5====="
   cp api_key ../
 
   cd ..
-echo " Step 7 "
-
-  echo "Waiting for Conjur server to come up"
-  # wait_for_conjur
 
   cd tests/conjur_variable
-echo " Step 9 "
+  echo "Waiting for Conjur server to come up"
+  # wait_for_conjur
 
   echo "Fetching SSL certs"
   fetch_ssl_certs
@@ -163,10 +131,7 @@ function setup_access_token {
   " > access_token
 }
 
-
-
 function run_test_cases {
-
 
   test_case="retrieve-variable-disable-verify-certs"
   docker-compose exec -T ansible bash -exc "
