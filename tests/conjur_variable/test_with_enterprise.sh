ab52ac4e477933a14d22daa4829221f8c947a1c8
Pooja Gangwar <pooja.gangwar@cyberark.com>
2022-05-18 16:22:48 +0530

    initial changes

--- tests/conjur_variable/test_with_enterprise.sh
+++ tests/conjur_variable/test_with_enterprise.sh
@@ -38,7 +38,7 @@ echo " Setup Policy "
 
     echo " ========load policy====="
     ./bin/cli conjur policy load root root.yml
-    ./bin/cli conjur policy load --replace root root.yml
+    # ./bin/cli conjur policy load --replace root root.yml
     echo " ========Set Variable value ansible/test-secret ====="
     ./bin/cli conjur variable values add ansible/test-secret test_secret_password
      echo " =======Set Variable value ansible/test-secret-in-file ====="
