534b477c7ea5e98a0d321421d0ab03d711a16b7e
Pooja Gangwar <pooja.gangwar@cyberark.com>
2022-05-23 19:17:46 +0530

    initial changes

--- tests/conjur_variable/test_with_enterprise.sh
+++ tests/conjur_variable/test_with_enterprise.sh
@@ -152,14 +152,14 @@ function run_test_cases {
     cd conjur_variable
     pwd
     ls
-    cd test_cases
-    pwd
-    ls
-    cd retrieve-variable
-    pwd
-    ls
+    # cd test_cases
+    # pwd
+    # ls
+    # cd retrieve-variable
+    # pwd
+    # ls
 
-    # ansible-playbook 'test_cases/${test_case}/playbook.yml'
+    ansible-playbook 'test_cases/${test_case}/playbook.yml'
 
     # py.test --junitxml='./junit/${test_case}' \
     #   --connection docker \
