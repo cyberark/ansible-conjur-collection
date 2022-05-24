62d72428f077ca998018fb87938c5ae16a6f7d39
Pooja Gangwar <pooja.gangwar@cyberark.com>
2022-05-24 09:30:20 +0530

    initial changes

--- tests/conjur_variable/test_with_enterprise.sh
+++ tests/conjur_variable/test_with_enterprise.sh
@@ -120,7 +120,7 @@ function main() {
        -e "CONJUR_AUTHN_LOGIN=admin" \
        -e "CONJUR_ADMIN_AUTHN_API_KEY=${CONJUR_ADMIN_AUTHN_API_KEY}" \
        -e "ANSIBLE_MASTER_AUTHN_API_KEY=${ANSIBLE_MASTER_AUTHN_API_KEY}" \
-       -e "ANSIBLE_CONJUR_CERT_FILE=/cyberark/tests/conjur-enterprise.pem" \
+       -e "ANSIBLE_CONJUR_CERT_FILE=/cyberark/tests/conjur_variable/conjur-enterprise.pem" \
        --workdir "/cyberark" \
        conjur_ansible:v1 \
 
