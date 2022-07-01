29d24b7c81a8b90b65f0eb1ef12834a3f03c3fbf
Pooja Gangwar <pooja.gangwar@cyberark.com>
2022-07-01 17:25:40 +0530

    change in script file

--- tests/conjur_variable/test.sh
+++ tests/conjur_variable/test.sh
@@ -136,14 +136,21 @@ function setup_conjur_enterprise() {
       "${cli_service}" \
       infinity
 
+    # echo "Authenticate Conjur CLI container(cli_service) : ${cli_service}"
+    # docker-compose exec "${cli_service}" \
+    #   /bin/bash -c "
+    #     if [ ! -e /root/conjur-demo.pem ]; then
+    #       yes 'yes' | conjur init -u ${CONJUR_APPLIANCE_URL} -a ${CONJUR_ACCOUNT}
+    #     fi
+    #     conjur authn login -u admin -p MySecretP@ss1
+    #     hostname -I
+    #   "
+
     echo "Authenticate Conjur CLI container(cli_service) : ${cli_service}"
     docker-compose exec "${cli_service}" \
       /bin/bash -c "
-        if [ ! -e /root/conjur-demo.pem ]; then
-          yes 'yes' | conjur init -u ${CONJUR_APPLIANCE_URL} -a ${CONJUR_ACCOUNT}
-        fi
-        conjur authn login -u admin -p MySecretP@ss1
-        hostname -I
+      conjur variable values add ansible/test-secret test_secret_password
+      conjur variable values add ansible/test-secret-in-file test_secret_in_file_password
       "
 
    echo "testing1"
