b7e10314211f48fa61c01ef1c26c944c083512ac
Pooja Gangwar <pooja.gangwar@cyberark.com>
2022-07-04 22:35:08 +0530

    changes in docker settings

--- roles/conjur_host_identity/tests/role_test.sh
+++ roles/conjur_host_identity/tests/role_test.sh
@@ -122,8 +122,8 @@ echo "get current directory"
             docker run \
             -d -t \
             --name ansible_container \
-            --volume "$(git rev-parse --show-toplevel)/roles/conjur_host_identity":/cyberark/cyberark.conjur.conjur-host-identity \
             --volume "$(git rev-parse --show-toplevel)/roles/conjur_host_identity/tests:cyberark/tests" \
+            --volume "$(git rev-parse --show-toplevel)/roles/conjur_host_identity":/cyberark/cyberark.conjur.conjur-host-identity \
             --network dap_net \
             -e "CONJUR_APPLIANCE_URL=https://conjur-master.mycompany.local" \
             -e "CONJUR_ACCOUNT=demo" \
