e9822205c3d08cb0072228d7ed54308b191a5e65
Pooja Gangwar <pooja.gangwar@cyberark.com>
2022-07-05 10:10:48 +0530

    changes in docker settings

--- roles/conjur_host_identity/tests/role_test.sh
+++ roles/conjur_host_identity/tests/role_test.sh
@@ -38,7 +38,6 @@ echo "get current directory"
             ls
 
             cp ../roles/conjur_host_identity/tests/policy/root.yml .
-            # cp ../policy/root.yml .
             ./bin/cli conjur policy load root root.yml
             echo " ========Set Variable value ansible/test-secret ====="
             ./bin/cli conjur variable values add ansible/target-password target_secret_password
@@ -67,8 +66,6 @@ echo "get current directory"
                 client \
                 -ec 'cp /root/conjur-demo.pem conjur-enterprise.pem
                 '
-
-                # conjur variable values add "ansible/var with spaces" var_with_spaces_secret_password
 
                 echo " ========testit 1====="
                 pwd
@@ -136,14 +133,9 @@ echo "get current directory"
             --workdir "/cyberark" \
             conjur_ansible:v1 \
 
-            # --volume "$(git rev-parse --show-toplevel)/roles/conjur_host_identity/tests:cyberark/tests" \
-
               echo "Running tests"
-              # containerid=sudo docker ps -aqf "name=ansible_container"
               containerid=$(docker ps -aqf "name=ansible_container")
               echo " container Id 1 is ${containerid} "
-            #   ansible_cid=$(docker-compose ps -q ansible)
-            #   echo " container Id 2 is ${ansible_cid} "
               run_test_cases
               echo " End of the tests "
 
@@ -168,152 +160,23 @@ function run_test_case {
       cd tests
       ansible-playbook test_cases/${test_case}/playbook.yml
     "
-    # if [ "${test_case}" == "configure-conjur-identity" ]
-    # then
-    #       docker exec -t ansible_container bash -exc "
-    #         cd tests
-    #         py.test --junitxml=./junit/${test_case} --connection docker -v test_cases/${test_case}/tests/test_default.py
-    #       "
-    # fi
+    if [ "${test_case}" == "configure-conjur-identity" ]
+    then
+    echo "---- testing 99 --"
+          docker exec -t ansible_container bash -exc "
+            cd tests
+            py.test --junitxml=./junit/${test_case} --connection docker -v test_cases/${test_case}/tests/test_default.py
+          "
+    fi
   else
     echo ERROR: run_test called with no argument 1>&2
     exit 1
   fi
 }
 
-# function run_test_cases {
-#  echo "---- testing 107 ----"
-#   for test_case in test_cases/*; do
-#     teardown_and_setup
-#     run_test_case "$(basename -- "$test_case")"
-#   done
-# }
-
 function teardown_and_setup {
   docker-compose up -d --force-recreate --scale test_app_ubuntu=2 test_app_ubuntu
   docker-compose up -d --force-recreate --scale test_app_centos=2 test_app_centos
 }
-
-# function run_test_case {
-# #   echo "---- testing 101 ${test_case} ----"
-# #   pwd
-# #   local test_case=$1
-# #   if [ -n "$test_case" ]
-# #   then
-#   echo "---- testing 102 ----"
-#   echo "hf_token ${hf_token}"
-#   echo "containerid ${containerid}"
-
-#     # docker exec -i "${containerid}" bin/bash -ec "
-#     # echo " pwd "
-#     # pwd
-#     # "
-
-#   echo "---- testing 110 ----"
-#     docker exec -t ansible_container bash -exc "
-#     ls
-#     echo " pwd "
-#     pwd
-#     "
-#         #   else
-#         #     echo ERROR: run_test called with no argument 1>&2
-#         #     exit 1
-#         #   fi
-#   echo "---- testing 120 ----"
-# }
-
-    # docker exec -t "${containerid}" bash -exc "
-    #   export HFTOKEN=${hf_token}
-    #   echo "---- testing 103 ----"
-    #   pwd
-    #   ls
-    #   cd tests
-    #   ansible-playbook test_cases/${test_case}/playbook.yml
-    # "
-    # if [ "${test_case}" == "configure-conjur-identity" ]
-    # then
-    #       docker exec "${containerid}" bash -ec "
-    #         cd tests
-    #         py.test --junitxml=./junit/${test_case} --connection docker -v test_cases/${test_case}/tests/test_default.py
-    #       "
-    # fi
-
-
-
-# function run_test_cases {
-#   for test_case in test_cases/*; do
-#     teardown_and_setup
-#     run_test_case "$(basename -- "$test_case")"
-#   done
-# }
-
-# function run_test_case {
-#   echo "---- testing pwd ${test_case} ----"
-#   pwd
-#   ls
-#   echo " this is hf_token value :- ${hf_token}"
-#     docker exec -t ansible_container bash -exc "
-#     echo " inside the ansible_container first"
-#     pwd
-#     ls
-#     "
-
-
-#   local test_case=$1
-#   if [ -n "$test_case" ]
-#   then
-#     #   docker exec -t ansible_container bash -exc
-#     #   cd tests
-#     docker exec ansible_container env HFTOKEN="${hf_token}" bash -ec "
-#     echo " inside the ansible_container first"
-#     pwd
-#     ls
-#     "
-#     if [ "${test_case}" == "configure-conjur-identity" ]
-#     then
-#           docker exec ansible_container bash -ec "
-#             echo " inside the ansible_container second "
-#             pwd
-#             ls
-#           "
-#     fi
-#   else
-#     echo ERROR: run_test called with no argument 1>&2
-#     exit 1
-#   fi
-# }
-
-
-
-# function run_test_cases {
-#   for test_case in test_cases/*; do
-#     run_test_case "$(basename -- "$test_case")"
-#   done
-# }
-# function run_test_case {
-#   local test_case=$1
-#   echo "---- testing ${test_case} ----"
-
-#   if [ -z "$test_case" ]; then
-#     echo ERROR: run_test called with no argument 1>&2
-#     exit 1
-#   fi
-
-#   docker exec -t ansible_container bash -exc "
-#     cd tests/conjur_variable
-
-#     # If env vars were provided, load them
-#     if [ -e 'test_cases/${test_case}/env_enterprise' ]; then
-#       . ./test_cases/${test_case}/env_enterprise
-#     fi
-
-#     # You can add -vvvv here for debugging
-#     ansible-playbook 'test_cases/${test_case}/playbook.yml'
-
-#     # py.test --junitxml='./junit/${test_case}' \
-#     #   --connection docker \
-#     #   -v 'test_cases/${test_case}/tests/test_default.py'
-#   "
-# }
 
 main
