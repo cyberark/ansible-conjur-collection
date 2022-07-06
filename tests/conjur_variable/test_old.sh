90d3ac9e1f6177f00852fb4f865aa4e4ac0361f6
Pooja Gangwar <pooja.gangwar@cyberark.com>
2022-07-06 20:32:42 +0530

    tsting

--- tests/conjur_variable/test_old.sh
+++ tests/conjur_variable/test_old.sh
@@ -177,8 +177,9 @@ function run_test_case {
     #     conjur variable values add "ansible/var with spaces" var_with_spaces_secret_password
     # "
 
+    # conjur policy load root ${policy_path}
+
     docker exec -t client bash -exc "
-        conjur policy load root ${policy_path}
         conjur variable values add ansible/test-secret test_secret_password
         conjur variable values add ansible/test-secret-in-file test_secret_in_file_password
         conjur variable values add "ansible/var with spaces" var_with_spaces_secret_password
@@ -229,14 +230,16 @@ function setup_conjur_enterprise() {
       ./bin/dap --provision-master
       ./bin/dap --provision-follower
 
-      docker-compose up -d --build client
+    #   docker-compose up -d --build client
 
       echo " ========load policy====="
       cp ../tests/conjur_variable/policy/root.yml .
 
+     ./bin/cli conjur policy load root root.yml
+
       setup_conjur_resources
 
-    #  ./bin/cli conjur policy load root root.yml
+
     #   echo " ========Set Variable value ansible/test-secret ====="
     #   ./bin/cli conjur variable values add ansible/test-secret test_secret_password
     #   echo " =======Set Variable value ansible/test-secret-in-file ====="
