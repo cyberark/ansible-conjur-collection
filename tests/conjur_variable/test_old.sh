42aadfa7e367824062cc616f47be14fd4e08772c
Pooja Gangwar <pooja.gangwar@cyberark.com>
2022-07-06 18:16:36 +0530

    tsting

--- tests/conjur_variable/test_old.sh
+++ tests/conjur_variable/test_old.sh
@@ -169,13 +169,19 @@ function run_test_case {
     echo " test  value ${enterprise} "
 
 
-    docker-compose exec -T "${cli_service}" bash -c "
-        conjur policy load root ${policy_path}
-        conjur variable values add ansible/test-secret test_secret_password
-        conjur variable values add ansible/test-secret-in-file test_secret_in_file_password
-        conjur variable values add "ansible/var with spaces" var_with_spaces_secret_password
-    "
-    }
+    # docker-compose exec -T "${cli_service}" bash -c "
+    #     conjur policy load root ${policy_path}
+    #     conjur variable values add ansible/test-secret test_secret_password
+    #     conjur variable values add ansible/test-secret-in-file test_secret_in_file_password
+    #     conjur variable values add "ansible/var with spaces" var_with_spaces_secret_password
+    # "
+    # }
+
+      ./bin/cli conjur policy load root root.yml
+      echo " ========Set Variable value ansible/test-secret ====="
+      ./bin/cli conjur variable values add ansible/test-secret test_secret_password
+      echo " =======Set Variable value ansible/test-secret-in-file ====="
+      ./bin/cli conjur variable values add ansible/test-secret-in-file test_secret_in_file_password
 
     function setup_admin_api_key {
     echo "Fetching admin API key"
