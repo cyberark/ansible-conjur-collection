737ce1e8d4fe3c041ece490b08208cacfd77b519
Rama Venkata Loya <ramavenkata.loya@cyberark.com>
2024-09-17 15:15:30 +0530

    updated the testcases to support all flavours

--- roles/conjur_host_identity/tests/test_cases/configure-conjur-identity/tests/test_default.py
+++ roles/conjur_host_identity/tests/test_cases/configure-conjur-identity/tests/test_default.py
@@ -2,6 +2,7 @@ from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
 import testinfra.utils.ansible_runner
+import os
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     '/cyberark/dev/inventory.tmp').get_hosts('testapp')
@@ -26,8 +27,17 @@ def test_is_conjurized(host):
     assert conf_file.exists
     assert conf_file.user == 'root'
 
-
 def test_retrieve_secret_with_summon(host):
-    result = host.check_output("summon --yaml 'DB_USERNAME: !var data/ansible/target-password' bash -c 'printenv DB_USERNAME'", shell=True)
+    # Get the environment variable
+    is_cloud = os.getenv('IS_CLOUD')
+
+    # Construct the path based on flavour
+    if is_cloud == 'true':
+        path = 'data/ansible/target-password'
+    else:
+        path = 'ansible/target-password'
+
+    # Run the command with the constructed path
+    result = host.check_output(f"summon --yaml 'DB_USERNAME: !var {path}' bash -c 'printenv DB_USERNAME'", shell=True)
 
     assert result == "target_secret_password"
